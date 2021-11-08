c184b4c7353f73c352ecd55dd2b6dabb78f423eb
RplayRENISH <88858106+Rplayoriginal@users.noreply.github.com>
2021-11-08 23:30:29 +0100

    Update stuff.py

--- bot/stuff.py
+++ bot/stuff.py
@@ -36,7 +36,7 @@ async def start(event):
             [Button.inline("HELP", data="ihelp")],
             [
                 Button.url(config.NAME, url="github.com/1Danish-00/"),
-                Button.url("DEVELOPER", url=g"t.me/{dev}"),
+                Button.inline("DEVELOPER", url=g"t.me/{dev}"),
             ],
         ],
     )
